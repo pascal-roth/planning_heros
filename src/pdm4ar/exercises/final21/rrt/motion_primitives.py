2741fb9dd7fed00a9bced14dcd32ee5bef83241c
Max Fruehauf <max@maxfruehauf.com>
2021-12-11 22:19:59 +0100

    Add reusing of motion primitives, however does not respect previous state conditions

--- src/pdm4ar/exercises/final21/rrt/motion_primitives.py
+++ src/pdm4ar/exercises/final21/rrt/motion_primitives.py
@@ -1,4 +1,4 @@
-from typing import List, Tuple
+from typing import List, Tuple, Dict
 import numpy as np
 from scipy.integrate import solve_ivp
 from dg_commons.sim.models.spacecraft import SpacecraftCommands, SpacecraftState, SpacecraftGeometry
@@ -7,60 +7,127 @@ import matplotlib.pyplot as plt
 import matplotlib
 
 from pdm4ar.exercises.final21.rrt.motion_constrains import MotionConstrains
-from pdm4ar.exercises.final21.rrt.trajectory import SpacecraftTrajectory
+from pdm4ar.exercises.final21.rrt.params import CONSTRAIN_VEL_ANG, DELTAT_LIMITS
 from scipy.spatial import KDTree
+
+
+class SpacecraftTrajectory:
+    def __init__(self, commands: List[SpacecraftCommands],
+                 states: List[SpacecraftState], t0: float, tf: float,
+                 dt: float):
+        self.commands = commands
+        self.states = states
+        assert t0 <= tf, f"SpacecraftTrajectory start time needs to be less than it's end time. Was: {tf} > {t0}"
+        self.t0 = t0
+        self.tf = tf
+        self.dt = dt
+
+    def get_cost(self) -> float:
+        return float(self.get_dist() / (self.tf - self.t0))
+
+    def get_dist(self) -> float:
+        dist = 0.
+        for i in range(1, len(self.states)):
+            p = self.states[i - 1]
+            c = self.states[i]
+            p_pos = np.array([p.x, p.y])
+            c_pos = np.array([c.x, c.y])
+            dist += np.linalg.norm(p_pos - c_pos)
+        return dist
+
+
+    def offset(self, x: float, y: float) -> 'SpacecraftTrajectory':
+        return SpacecraftTrajectory(self.commands.copy(), [
+            SpacecraftState(state.x + x, state.y + y, state.psi, state.vx,
+                            state.vy, state.dpsi) for state in self.states
+        ], self.t0, self.tf, self.dt)
+
+
+class TrajectoryGroup:
+    def __init__(self, trajectories: List[SpacecraftTrajectory]):
+        self.trajectories = trajectories
+        self.final_pos = np.array([[t.states[-1].x, t.states[-1].y]
+                                   for t in self.trajectories])
+        self.kdtree = KDTree(self.final_pos)
 
 
 class MotionPrimitives:
     def __init__(self,
                  spacecraft_geometry: SpacecraftGeometry,
-                 time_step: float = 0.1):
+                 steps: int = 10):
         self.sg: SpacecraftGeometry = spacecraft_geometry
-        self.dt: float = time_step
+        self.steps = steps
 
         self.motion_constrains: MotionConstrains = MotionConstrains()
         self.primitives: List[SpacecraftTrajectory] = []
-        self.kdtree: KDTree
         self.max_distance_covered = self._max_distance_covered()
 
-    def generate(self,
-                 spacecraft_t0: SpacecraftState,
-                 steps: int = 10) -> List[SpacecraftTrajectory]:
-        """Generates all motion primitives for a given initial state
+        self.primitive_database: Dict[str, TrajectoryGroup] = dict()
 
-        Args:
-            spacecraft_t0 (SpacecraftState): initial state for the primitives
-            steps (int, optional): Number of divisions for input accelerations. Defaults to 10.
+    def distance(self, start: SpacecraftState,
+                 end: SpacecraftState) -> Tuple[float, SpacecraftTrajectory]:
+        discrete = self._discretize_state(start)
 
-        Returns:
-            List[SpacecraftTrajectory]: List of all generated trajectories
-        """
+        # get trajectory group and create it if not present
+        discrete_repr = repr(discrete)
+        if discrete_repr not in self.primitive_database:
+            print(f"building new TrajectoryGroup for {discrete}")
+            self.primitive_database[discrete_repr] = TrajectoryGroup(
+                self._generate_trajectories(discrete))
+        traj_group = self.primitive_database[discrete_repr]
+
+        # find closest end state
+        _, closest_idx = traj_group.kdtree.query(
+            [end.x - start.x, end.y - start.y])
+        trajectory: SpacecraftTrajectory = traj_group.trajectories[closest_idx]
+
+        trajectory = trajectory.offset(start.x, start.y)
+        return trajectory.get_cost(), trajectory
+
+    def _discretize_state(self, state: SpacecraftState) -> SpacecraftState:
+        def closest(value: float, steps: np.ndarray) -> float:
+            norm = (steps - value)**2
+            c_idx = np.argmin(norm)
+            return steps[c_idx]
+
+        limit_vel = self.motion_constrains.limit_vel
+        limit_dpsi = self.motion_constrains.limit_dpsi
+        steps = 5
+        return SpacecraftState(
+            x=0,
+            y=0,
+            psi=closest(state.psi,
+                        np.linspace(0, 2 * np.pi, steps)[:-1]),
+            vx=closest(state.vx, np.linspace(limit_vel[0], limit_vel[1],
+                                             steps)),
+            vy=closest(state.vy, np.linspace(limit_vel[0], limit_vel[1],
+                                             steps)),
+            dpsi=closest(state.dpsi,
+                         np.linspace(limit_dpsi[0], limit_dpsi[1], steps)))
+
+    def _generate_trajectories(
+            self, state: SpacecraftState) -> List[SpacecraftTrajectory]:
         input_limits = self.motion_constrains.limit_acc
-        acc = np.linspace(input_limits[0], input_limits[1], steps)
+        acc = np.linspace(input_limits[0], input_limits[1], self.steps)
+        dts = np.linspace(DELTAT_LIMITS[0], DELTAT_LIMITS[1], self.steps)
         acc_left, acc_right = np.meshgrid(acc, acc)
-        self.primitives.clear()
-        final_pos = np.zeros((acc_left.shape[0] * acc_left.shape[1], 2))
+        primitives = []
 
-        for i in range(acc_left.shape[0]):
-            for j in range(acc_left.shape[1]):
-                command = SpacecraftCommands(acc_left[i, j], acc_right[i, j])
-                trajectory = self._get_trajectory(spacecraft_t0, command)
-                self.primitives.append(trajectory)
-                final_pos[i * acc_left.shape[0] + j] = np.array(
-                    [trajectory.states[-1].x, trajectory.states[-1].y])
+        for k in range(dts.shape[0]):
+            dt = dts[k]
+            for i in range(acc_left.shape[0]):
+                for j in range(acc_left.shape[1]):
+                    command = SpacecraftCommands(acc_left[i, j], acc_right[i,
+                                                                           j])
+                    trajectory = self._get_trajectory(state, command, dt)
+                    primitives.append(trajectory)
+        return primitives
 
-        self._kdtree = KDTree(final_pos)
-        return self.primitives
-
-    def closest(self,
-                pos: np.ndarray) -> Tuple[SpacecraftTrajectory, np.ndarray]:
-        _, closest_idx = self._kdtree.query(pos)
-        closest = self.primitives[closest_idx]
-        end_pos = np.array([closest.states[-1].x, closest.states[-1].y])
-        return self.primitives[closest_idx], end_pos
-
-    def _get_trajectory(self, spacecraft_t0: SpacecraftState,
-                        u: SpacecraftCommands) -> SpacecraftTrajectory:
+    def _get_trajectory(self,
+                        spacecraft_t0: SpacecraftState,
+                        u: SpacecraftCommands,
+                        tf: float,
+                        dt: float = 0.1) -> SpacecraftTrajectory:
         # express initial state
         y0 = np.array([
             spacecraft_t0.x, spacecraft_t0.y, spacecraft_t0.vx,
@@ -69,8 +136,13 @@ class MotionPrimitives:
 
         def dynamics(t, y):
             px, py, vx, vy, psi, dpsi = y
+            # only apply acceleration in the beginning
+            # if t <= dt:
             acc_left = self.motion_constrains.apply_acc_limit(u.acc_left)
             acc_right = self.motion_constrains.apply_acc_limit(u.acc_right)
+            # else:
+            #     acc_left = 0
+            #     acc_right = 0
 
             acc_sum = acc_right + acc_left
             acc_diff = acc_right - acc_left
@@ -95,10 +167,7 @@ class MotionPrimitives:
             ret[5] = ddpsi
             return ret
 
-        sol = solve_ivp(fun=dynamics,
-                        t_span=(0.0, self.dt),
-                        y0=y0,
-                        vectorized=True)
+        sol = solve_ivp(fun=dynamics, t_span=(0.0, tf), y0=y0, vectorized=True)
 
         assert sol.success, f"Solving the IVP for ({u.acc_left}, {u.acc_right}) failed"
         states: List[SpacecraftState] = [None] * sol.y.shape[1]
@@ -110,7 +179,7 @@ class MotionPrimitives:
                                         vx=s[2],
                                         vy=s[3],
                                         dpsi=s[5])
-        return SpacecraftTrajectory([u], states, 0, self.dt)
+        return SpacecraftTrajectory([u], states, 0., tf, dt)
 
     def _max_distance_covered(self) -> float:
         fastest_state = SpacecraftState(x=0,
@@ -119,16 +188,16 @@ class MotionPrimitives:
                                         vx=self.motion_constrains.limit_vel[1],
                                         vy=self.motion_constrains.limit_vel[1],
                                         dpsi=0)
-        self.generate(fastest_state, steps=2)
-        return np.max([trajectory.get_cost() for trajectory in self.primitives])
+        primitives = self._generate_trajectories(fastest_state)
+        return np.max([trajectory.get_cost() for trajectory in primitives])
 
-    def plot_primitives(self):
-        plt.figure()
-        for trajectory in self.primitives:
-            x = [s.x for s in trajectory.states]
-            y = [s.y for s in trajectory.states]
-            plt.plot(x, y)
-        plt.show()
+    # def plot_primitives(self):
+    #     plt.figure()
+    #     for trajectory in self.primitives:
+    #         x = [s.x for s in trajectory.states]
+    #         y = [s.y for s in trajectory.states]
+    #         plt.plot(x, y)
+    #     plt.show()
 
 
 if __name__ == "__main__":
@@ -136,5 +205,5 @@ if __name__ == "__main__":
     sg = SpacecraftGeometry.default()
     mp = MotionPrimitives(sg)
     spacecraft_t0 = SpacecraftState(x=0, y=0, psi=0, vx=.5, vy=0, dpsi=0)
-    mp.generate(spacecraft_t0)
+    mp.distance(spacecraft_t0)
     mp.plot_primitives()
--- src/pdm4ar/exercises/final21/rrt/rrt.py
+++ src/pdm4ar/exercises/final21/rrt/rrt.py
@@ -10,7 +10,7 @@ from dg_commons.sim.models.obstacles import StaticObstacle
 from dg_commons.sim.models.spacecraft import SpacecraftState
 from dg_commons.maps.shapely_viz import ShapelyViz
 from dg_commons.sim.simulator_visualisation import ZOrders
-from pdm4ar.exercises.final21.rrt.motion_primitives import MotionPrimitives
+from pdm4ar.exercises.final21.rrt.motion_primitives import MotionPrimitives, SpacecraftTrajectory
 from pdm4ar.exercises.final21.rrt.params import MOTION_PRIMITIVE_INPUT_DIVISIONS, STEERING_MAX_DIST
 
 from pdm4ar.exercises.final21.rrt.sampler import Sampler
@@ -18,8 +18,6 @@ from pdm4ar.exercises.final21.rrt.distance import Distance, DistanceMetric
 from pdm4ar.exercises.final21.rrt.cost import euclidean_cost
 
 import networkx as nx
-
-from pdm4ar.exercises.final21.rrt.trajectory import SpacecraftTrajectory
 
 
 class Node:
@@ -43,8 +41,7 @@ class RRT:
                  sg: SpacecraftGeometry,
                  n_samples: int = 500,
                  distance_metric: DistanceMetric = DistanceMetric.L2,
-                 radius: Optional[float] = 5.0,
-                 dt: float = 0.1):
+                 radius: Optional[float] = 30):
         """
         :param goal:                Goal Polygon
         :param static_obstacles:    Obstacles in the given environment, note that obstacle 0 is the boundary of the
@@ -71,8 +68,8 @@ class RRT:
 
         self.tree = nx.DiGraph()
 
-        self.dt = dt
-        self.motion_primitives = MotionPrimitives(self.sg, 1)
+        self.motion_primitives = MotionPrimitives(
+            self.sg, MOTION_PRIMITIVE_INPUT_DIVISIONS)
         self.radius = self.motion_primitives.max_distance_covered
 
     def plan_path(self, spacecraft_state: SpacecraftState, plot: bool = True):
@@ -99,7 +96,7 @@ class RRT:
         # TODO: get optimal plan
 
         # TODO: reuse part of the sample state has not been changed due to moving obstacle or movement of the spacecraft
-        self.tree = {}
+        # self.tree = {}
 
     def refine_path(self, n_samples: int) -> None:
         # function not tested
@@ -124,15 +121,19 @@ class RRT:
         # calculate x_new as the point closest to x_rand that can be reached with a movement primitive without
         # collision, in the following it is assumed that a collision free path is possible
         x_nearest = self._nearest_on_tree(x_rand)
-        if np.linalg.norm(x_nearest.pos - x_rand) > self.radius:
-            return
-        self.motion_primitives.generate(x_nearest.state,
-                                        MOTION_PRIMITIVE_INPUT_DIVISIONS)
-        trajectory, end = self.motion_primitives.closest(x_rand)
-        x_new_dist = self.distance.dist(end, x_rand)
-        if x_new_dist > STEERING_MAX_DIST:
-            print(f"rejected: x_new_dist: {x_new_dist}")
-            return
+        # if np.linalg.norm(x_nearest.pos - x_rand) > self.radius:
+        #     return
+        goal_state = SpacecraftState(x=x_rand[0],
+                                     y=x_rand[1],
+                                     psi=0,
+                                     vx=0,
+                                     vy=0,
+                                     dpsi=0)
+        x_new_dist, trajectory = self.motion_primitives.distance(
+            x_nearest.state, goal_state)
+        # if trajectory.get_dist() > STEERING_MAX_DIST:
+        #     print(f"rejected: x_new_dist: {x_new_dist}")
+        #     return
 
         x_new = Node(state=trajectory.states[-1],
                      cost=x_nearest.cost + trajectory.get_cost())
@@ -141,11 +142,10 @@ class RRT:
         # TODO: update the radius dynamically with maximum distance that can be reached by movement primitive and
         #  that log(n) samples are included in it
         near_nodes = self._near_on_tree(
-            x_new, self.radius,
-            lambda n1, n2: self.distance.dist(n1.pos, n2.pos))
+            x_new, self.radius, lambda n1, n2: self.motion_primitives.distance(
+                n1.state, n2.state)[0])
 
         self.tree.add_node(x_new)
-        print(f"added node {x_new}")
 
         # check if x_near in the tree otherwise set it to start
         # additional_cost = 0
@@ -161,7 +161,8 @@ class RRT:
         # check for all samples within radius which results in the smallest cost to reach the new sample
         for x_near in near_nodes:
             collision_free = True
-            line_cost = self.distance.dist(x_near.pos, x_new.pos)
+            line_cost, _ = self.motion_primitives.distance(
+                x_near.state, x_new.state)
             if collision_free and x_near.cost + line_cost < c_min:
                 x_min = x_near
                 c_min = x_near.cost + line_cost
@@ -172,11 +173,15 @@ class RRT:
         for x_near in near_nodes:
             # TODO: also add collision check
             collision_free = True
-            line_cost = c_min + self.distance.dist(x_new.pos, x_near.pos)
-            if line_cost < x_near.cost:
+            motion_cost, trajectory = self.motion_primitives.distance(
+                x_new.state, x_near.state)
+            line_cost = c_min + motion_cost
+            if c_min + motion_cost < x_near.cost:
                 x_parent = self.tree.predecessors(x_near)
-                self.tree.remove_edge(x_parent, x_near)
-                self.tree.add_edge(x_new, x_near)
+                # TODO: somehow without this check we get an error, investigate why
+                if self.tree.has_edge(x_parent, x_near):
+                    self.tree.remove_edge(x_parent, x_near)
+                self.tree.add_edge(x_new, x_near, trajectory=trajectory)
 
     def _nearest_on_tree(self, pos: np.ndarray) -> Node:
         # TODO replace brute force distance search
@@ -219,7 +224,7 @@ class RRT:
 
     def _plotter(self, ax):
         pos = {node: node.pos for node in self.tree.nodes}
-        nx.draw_networkx_nodes(self.tree, pos)
+        nx.draw_networkx_nodes(self.tree, pos, node_size=100)
         for from_node, to_node, data in self.tree.edges(data=True):
             if "trajectory" in data:
                 trajectory: SpacecraftTrajectory = data["trajectory"]
