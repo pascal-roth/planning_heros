50395882b95d15c3843ed386f649f2925612c5da
Roth, Pascal <pascal.roth.18@stud.tu-darmstadt.de>
2021-12-11 11:48:06 +0100

    RRT with kdtree and other improvements

--- src/pdm4ar/exercises/final21/rrt/distance.py
+++ src/pdm4ar/exercises/final21/rrt/distance.py
@@ -1,13 +1,7 @@
 import numpy as np
 import enum
 from typing import Optional, Callable, Union, Tuple
-
-
-class DistanceMethod(enum.Enum):
-    # brute force distance calculation
-    BF = 0,
-    # kdtree
-    KDTREE = 1,
+from scipy.spatial import KDTree
 
 
 class DistanceMetric(enum.Enum):
@@ -16,36 +10,21 @@ class DistanceMetric(enum.Enum):
 
 
 class Distance:
-    def __init__(self,
-                 method: DistanceMethod = DistanceMethod.BF,
-                 metric: DistanceMetric = DistanceMetric.L2):
+    def __init__(self,metric: DistanceMetric = DistanceMetric.L2):
         """
         :param metric
-        :param method:
         """
-        self.method: DistanceMethod = method
         self.metric: DistanceMetric = metric
 
         self.metric_fct: Callable
         self._get_metric()
+        self.tree: KDTree
 
-    def get_nn(self, point_idx: int, point_cloud: np.ndarray, radius: Optional[float] = None) -> \
-            Union[Tuple[np.ndarray, np.ndarray], np.ndarray]:
-        distance = self._get_distance(point_idx, point_cloud)
-        # if radius is defined, give all samples within radius, otherwise just the clostest idx of the pointcloud
-        if radius:
-            distance_mask = [distance_current < radius for distance_current in distance]
-            distance_idx = np.arange(len(distance))
-            return np.argmin(distance), distance_idx[distance_mask]
-        else:
-            return np.argmin(distance)
+    def init_tree(self, samples: np.ndarray) -> None:
+        self.tree = KDTree(samples)
 
-    def _get_distance(self, point_idx: int, point_cloud: np.ndarray, ):
-        if self.method == DistanceMethod.BF:
-            distance = [self.metric_fct(point_cloud[point_idx] - pc_entry) for pc_entry in point_cloud]
-        else:
-            raise NotImplementedError(f'Distance Method {self.method} not implemented')
-
+    def get_distance(self, point_idx: int, point_cloud: np.ndarray, ):
+        distance = [self.metric_fct(point_cloud[point_idx] - pc_entry) for pc_entry in point_cloud]
         # the point we compare the distance with is already included in the pointcloud -> set its distance to inf
         distance[point_idx] = np.inf
         return distance
--- src/pdm4ar/exercises/final21/rrt/rrt.py
+++ src/pdm4ar/exercises/final21/rrt/rrt.py
@@ -1,3 +1,4 @@
+import time
 import numpy as np
 import matplotlib
 import matplotlib.pyplot as plt
@@ -10,37 +11,34 @@ from dg_commons.maps.shapely_viz import ShapelyViz
 from dg_commons.sim.simulator_visualisation import ZOrders
 
 from pdm4ar.exercises.final21.rrt.sampler import Sampler
-from pdm4ar.exercises.final21.rrt.distance import Distance, DistanceMethod, DistanceMetric
+from pdm4ar.exercises.final21.rrt.distance import Distance, DistanceMetric
 from pdm4ar.exercises.final21.rrt.cost import euclidean_cost
 
 
 class RRT:
     def __init__(self, goal: PolygonGoal,
                  static_obstacles: Sequence[StaticObstacle],
-                 n_init: int = 1,
-                 n_total: int = 1000,
-                 distance_method: DistanceMethod = DistanceMethod.BF, 
-                 distance_metric: DistanceMetric = DistanceMetric.L2, 
-                 radius: Optional[float] = 10.0):
+                 n_samples: int = 1000,
+                 distance_metric: DistanceMetric = DistanceMetric.L2,
+                 radius: Optional[float] = 5.0):
         """
         :param goal:                Goal Polygon
         :param static_obstacles:    Obstacles in the given environment, note that obstacle 0 is the boundary of the
                                     environment !!!
-        :param n_init:              init point cloud with random n samples from the environment using Halton Sequence
+        :param n_samples:           point cloud with random n samples from the environment using Halton Sequence
         """
 
         self.goal: PolygonGoal = goal
         self.static_obstacles: Sequence[StaticObstacle] = static_obstacles
 
         # init sampler and point cloud
-        self.n_init: int = n_init
-        self.n_total: int = n_total
-        self.sampler: Sampler = Sampler(static_obstacles, n_samples=n_init)
-        self.sampler.plot_samples(self.goal)
+        self.n_samples: int = n_samples
+        self.sampler: Sampler = Sampler(static_obstacles, n_samples=n_samples)
+        # self.sampler.plot_samples(self.goal)
 
         # init distance calculation (atm with brute force distance and euclidean distance)
         self.radius: Optional[float] = radius
-        self.distance = Distance(distance_method, distance_metric)
+        self.distance = Distance(distance_metric)
 
         # TODO: init movement primitives
         # TODO: init a proper cost class, that takes time and distance into account
@@ -50,90 +48,114 @@ class RRT:
         self.sample_state: Dict[int, SampleState] = {}
 
     def plan_path(self, spacecraft_state: SpacecraftState, plot: bool = True):
-        # construct a course path map for the initial point cloud, possible helpful for increasing the speed
-        x_start = np.expand_dims(np.array([spacecraft_state.x, spacecraft_state.y]), axis=0)
-        self.sampler.add_sample(x_start)
-        x_start_idx = self.sampler.point_cloud_idx_latest-1
-        self.sample_state[x_start_idx] = SampleState(parent=x_start_idx, cost=0)
-        distance = self.distance._get_distance(x_start_idx, point_cloud=self.sampler.pc2array())
+        t_start = time.time()
+
+        # add start point to point-cloud (pc)
+        self._add_start_to_pc(spacecraft_state)
+
+        # build kdtree and determine distance of each point from Spacecraft
+        self.distance.init_tree(self.sampler.pc2array())
+        distance = self.distance.get_distance(self.sampler.point_cloud_idx_latest-1,
+                                              point_cloud=self.sampler.pc2array())
         distance_idx_sorted = np.argsort(distance)
-        for idx in range(self.n_init):
-            self._update_graph(distance_idx_sorted[idx])
+
+        for idx in distance_idx_sorted:
+            self._update_graph(idx)
+
+        t_end = time.time()
+        print(f'Building RRT* map in {np.round(t_end - t_start, decimals=4)}s')
 
         if plot:
-            matplotlib.use('TkAgg')
-            ax = plt.gca()
-            # shapely_viz = ShapelyViz(ax)
-            #
-            # for s_obstacle in self.static_obstacles:
-            #     shapely_viz.add_shape(s_obstacle.shape, color=s_obstacle.geometry.color,
-            #                           zorder=ZOrders.ENV_OBSTACLE)
-            # shapely_viz.add_shape(self.goal.get_plottable_geometry(), color="orange", zorder=ZOrders.GOAL, alpha=0.5)
-            # ax = shapely_viz.ax
-            ax.autoscale()
-            ax.set_facecolor('k')
-            ax.set_aspect("equal")
-
-        # add points to path map until n_total is reached
-        for idx in range(self.n_total - self.n_init):
-            x_rand_idx = self.sampler.draw_samples()[0]
-            self._update_graph(x_rand_idx)
-
-            if plot:
-                pc_points = [point for point in self.sample_state.keys()]
-                pc_parents = [state.parent for state in self.sample_state.values()]
-                for i in range(len(pc_points)):
-                    pc_point = self.sampler.point_cloud[pc_points[i]]
-                    pc_parent = self.sampler.point_cloud[pc_parents[i]]
-                    ax.plot((pc_point[0], pc_parent[0]), (pc_point[1], pc_parent[1]), color='r')
-                ax.set_title(f'added {idx} samples')
-                plt.pause(0.05)
-            plt.show()
+            ax = self._init_plot()
+            self._plotter(ax)
 
         # TODO: get optimal plan
 
-    def _update_graph(self, x_rand_idx):
-        # NOTE: in theory, if x_new is updated from x_rand, BF has to be executed twice, however if we say its close,
-        #       the approximation makes 1 BF unnecessary
-        # x_near_idx = self.distance.get_nn(x_rand_idx, point_cloud=self.sampler.pc2array())
+        # TODO: reuse part of the sample state has not been changed due to moving obstacle or movement of the spacecraft
+        self.sample_state = {}
+
+    def refine_path(self, n_samples: int) -> None:
+        # function not tested
+        x_rand_idx = self.sampler.draw_samples(n_samples)
+        self.distance.init_tree(self.sampler.pc2array())
+
+        for idx in range(n_samples):
+            self._update_graph(x_rand_idx)
+
+    def _add_start_to_pc(self, spacecraft_state: SpacecraftState) -> None:
+        x_start = np.expand_dims(np.array([spacecraft_state.x, spacecraft_state.y]), axis=0)
+        self.sampler.add_sample(x_start)
+        self.sample_state[self.sampler.point_cloud_idx_latest-1] = SampleState(
+            parent=self.sampler.point_cloud_idx_latest-1, cost=0)
+
+    def _update_graph(self, x_idx):
+        x_rand = self.sampler.point_cloud[x_idx]
 
         # TODO: calculate x_new as the point closest to x_rand that can be reached with a movement primitive without
         #  collision, in the following it is assumed that a collision free path is possible
-        x_new_idx = x_rand_idx
+        x_new = x_rand
 
         # collect closest sample and all samples within distance eth=radius
-        x_near_idx, X_near_idx = self.distance.get_nn(x_new_idx, point_cloud=self.sampler.pc2array(),
-                                                      radius=self.radius)
+        _, x_near_idx = self.distance.tree.query(x_new, k=2)
+        x_near_idx = x_near_idx[1]
+        X_near_idx = self.distance.tree.query_ball_point(x_new, r=self.radius)
+        # TODO: update the radius dynamically with maximum distance that can be reached by movement primitive and
+        #  that log(n) samples are included in it
+
+        # check if x_near in the sample_state otherwise set it to start
+        additional_cost = 0
+        if not self.sample_state.get(x_near_idx):
+            x_near_idx = self.sampler.point_cloud_idx_latest-1
+            if x_near_idx not in X_near_idx:  # additional cost just necessary until movement primitives and proper cost
+                additional_cost = 1000
 
         # init x_min and its cost
         x_min_idx = x_near_idx
-        c_min = self.sample_state[x_near_idx].cost + euclidean_cost(self.sampler.point_cloud[x_near_idx],
-                                                                    self.sampler.point_cloud[x_new_idx])
+        c_min = self.sample_state[x_near_idx].cost + euclidean_cost(self.sampler.point_cloud[x_near_idx], x_new) + additional_cost
 
         # check for all samples within radius which results in the smallest cost to reach the new sample
-        for x_near_current in X_near_idx:
-
-            # if not self.sample_state.get(x_near_current):
-            #     continue
-
-            cost = self.sample_state[x_near_current].cost + euclidean_cost(self.sampler.point_cloud[x_near_current],
-                                                                           self.sampler.point_cloud[x_new_idx])
+        for x_near_i in X_near_idx:
+            if not self.sample_state.get(x_near_i):
+                continue
+            cost = self.sample_state[x_near_i].cost + euclidean_cost(self.sampler.point_cloud[x_near_i], x_new)
             if cost < c_min:  # TODO: also add collision check
-                x_min_idx = x_near_current
+                x_min_idx = x_near_i
                 c_min = cost
 
-        self.sample_state[x_new_idx] = SampleState(parent=x_min_idx, cost=c_min)
+        self.sample_state[x_idx] = SampleState(parent=x_min_idx, cost=c_min)
 
         # rebuild tree s.t. samples that can be reached with a smaller cost from the x_new are updated
-        for x_near_current in X_near_idx:
+        for x_near_i in X_near_idx:
+            if not self.sample_state.get(x_near_i):
+                continue
+            cost = c_min + euclidean_cost(x_new, self.sampler.point_cloud[x_near_i])
+            if cost < self.sample_state[x_near_i].cost:  # TODO: also add collision check
+                self.sample_state[x_near_i].parent = x_idx
 
-            # if not self.sample_state.get(x_near_current):
-            #     continue
+    def _init_plot(self):
+        matplotlib.use('TkAgg')
+        ax = plt.gca()
+        shapely_viz = ShapelyViz(ax)
 
-            cost = c_min + euclidean_cost(self.sampler.point_cloud[x_new_idx], self.sampler.point_cloud[x_near_current])
+        for s_obstacle in self.static_obstacles:
+            shapely_viz.add_shape(s_obstacle.shape, color=s_obstacle.geometry.color,
+                                  zorder=ZOrders.ENV_OBSTACLE)
+        shapely_viz.add_shape(self.goal.get_plottable_geometry(), color="orange", zorder=ZOrders.GOAL, alpha=0.5)
+        ax = shapely_viz.ax
+        ax.autoscale()
+        ax.set_facecolor('k')
+        ax.set_aspect("equal")
+        return ax
 
-            if cost < self.sample_state[x_near_current].cost:
-                self.sample_state[x_near_current].parent = x_new_idx
+    def _plotter(self, ax):
+        pc_points = [point for point in self.sample_state.keys()]
+        pc_parents = [state.parent for state in self.sample_state.values()]
+        for i in range(len(pc_points)):
+            pc_point = self.sampler.point_cloud[pc_points[i]]
+            pc_parent = self.sampler.point_cloud[pc_parents[i]]
+            ax.plot((pc_point[0], pc_parent[0]), (pc_point[1], pc_parent[1]), color='r')
+        ax.set_title(f'added {self.n_samples} samples')
+        plt.show()
 
 
 class SampleState:
